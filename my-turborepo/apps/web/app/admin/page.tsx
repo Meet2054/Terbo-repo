8bba3db4b13152cd396a7b53a53d899d708bd6bf
Meet2054 <meetaparekh2003@gmail.com>
2024-06-02 23:42:53 +0530

    update

---
+++ my-turborepo/apps/web/app/admin/page.tsx
@@ -0,0 +1,11 @@
+import Admin from '@repo/ui/admin'
+const page = () => {
+    return (
+        <div>
+            <h1>Hii from admin page </h1>
+            <Admin />
+        </div>
+    )
+}
+
+export default page
---
+++ my-turborepo/packages/ui/src/admin.tsx
@@ -0,0 +1,10 @@
+
+const Admin = () => {
+  return (
+    <div className='w-[50px] h-[50px] rounded-md border-2 border-black items-center bg-green'>
+      <h1>Admin page</h1>
+    </div>
+  )
+}
+
+export default Admin
